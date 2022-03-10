d97d895488806894a401592b8fdb131a0e34b217
VictoriaLizCor <victoria.lizcor@gmail.com>
2022-03-10 17:11:45 -0600

    changes on header

--- Public/javascripts/fixedScroll.js
+++ Public/javascripts/fixedScroll.js
@@ -1,13 +1,13 @@
-const nav = document.querySelector('nav');
-const header = document.querySelector('.header_background');
+const nav = document.querySelector('.navbar');
+const headerBackground = document.querySelector('.slogan').getBoundingClientRect();;
 window.addEventListener('scroll', fixNav);
   
 function fixNav() {
     //if(window.scrollY > heigth) {
-    if(window.scrollY > header.offsetHeight) {
-        nav.classList.add('active');
-    } else {
+    if(window.scrollY < headerBackground.bottom - (headerBackground.bottom)/2) {
         nav.classList.remove('active');
+    } else {
+        nav.classList.add('active');
     }
 }
 
