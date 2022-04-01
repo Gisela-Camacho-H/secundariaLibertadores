d07d79a91ed5b8c4cda1f74be9c1aa94ec590d3e
Cinthia Gisela Camacho <gis25cam@gmail.com>
2022-04-01 15:48:17 -0600

    change validations

--- Public/javascripts/contactoAddValidation.js
+++ Public/javascripts/contactoAddValidation.js
@@ -10,5 +10,23 @@ window.addEventListener("load", function() {
         if(campoNombre.value == ""){
             document.getElementById("errorNombre").innerHTML = "El campo de nombre tiene que estar completo";
         }
+
+        let campoEmail = document.querySelector("input.email");
+        
+        if(campoEmail.value == ""){
+            document.getElementById("errorEmail").innerHTML = "El campo de correo tiene que estar completo";
+        }
+
+        let campoTelefono = document.querySelector("input.telefono");
+        
+        if(campoTelefono.value == ""){
+            document.getElementById("errorTelefono").innerHTML = "El campo de telefono tiene que estar completo";
+        }
+
+        let campoConsulta = document.querySelector("textarea.consulta");
+        
+        if(campoConsulta.value == ""){
+            document.getElementById("errorConsulta").innerHTML = "El campo de consulta tiene que estar completo";
+        }
     });
 })
